ae18b16ee7c8cd9f025903a046a3d28448defaf0
Agustin Sampayo <agustines82@gmail.com>
2022-10-06 11:51:23 -0300

    creo la variable de estado cuidad que guardarà el valor de la ciudad buscada

--- src/components/Formulario.jsx
+++ src/components/Formulario.jsx
@@ -1,7 +1,19 @@
+import { useEffect, useState } from "react";
 import { InputGroup, Form, Button, Container, Row, Col } from "react-bootstrap";
 import Informacion from "./Informacion";
 
 const Formulario = () => {
+    //en esta variable meteremos los datos buscados en el input del form en el evento change (funcion handleChange).
+    const [ciudad, setCiudad] = useState("");
+
+    useEffect(() => {
+        // consultarAPI();
+    }, []);
+
+    const handleChange = (e) => {
+        setCiudad(e.target.value);
+    };
+
     return (
         <>
             <Container>
@@ -9,7 +21,12 @@ const Formulario = () => {
                     <Col md={6}>
                         <Form className="p-2 my-3">
                             <InputGroup className="mb-3">
-                                <Form.Control placeholder="Buscar ciudad" aria-label="Recipient's username" aria-describedby="basic-addon2" />
+                                <Form.Control
+                                    placeholder="Buscar ciudad"
+                                    aria-label="Recipient's username"
+                                    aria-describedby="basic-addon2"
+                                    onChange={handleChange}
+                                />
                                 <Button type="submit">Buscar</Button>
                             </InputGroup>
                         </Form>
