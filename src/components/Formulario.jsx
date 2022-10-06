e184d819a1c8d0c0c6c069155d28f9fdeb598381
Agustin Sampayo <agustines82@gmail.com>
2022-10-06 15:56:17 -0300

    casi

--- src/components/Formulario.jsx
+++ src/components/Formulario.jsx
@@ -5,13 +5,48 @@ import Informacion from "./Informacion";
 const Formulario = () => {
     //en esta variable meteremos los datos buscados en el input del form en el evento change (funcion handleChange).
     const [ciudad, setCiudad] = useState("");
+    //en esta variable metemos el valor de ciudad para que cuando haga la peticion a la Api la ciudad vaya escrita completa
+    const [ciudadOk, setCiudadOk] = useState("");
+    //en esta variable meteremos los datos que obtendremos de la API y la enviaremos por props al componente Informacion (la inicializo con valores vacios)
+    const [datosClima, setDatosClima] = useState({
+        coord: {},
+        weather: [{ id: "", main: "", description: "", icon: "" }],
+        base: "",
+        main: { temp: "", feels_like: "", temp_min: "", temp_max: "", pressure: "", humidity: "" },
+        visibility: "",
+        wind: { speed: "", deg: "" },
+        clouds: { all: "" },
+        dt: "",
+        sys: { type: "", id: "", country: "", sunrise: "", sunset: "" },
+        timezone: "",
+        id: "",
+        name: "",
+        cod: "",
+    });
 
     useEffect(() => {
-        // consultarAPI();
-    }, []);
+        if (ciudad.trim().length > 0) {
+            consultarAPI().then((respuestaDatosClima) => {
+                setDatosClima(respuestaDatosClima);
+            });
+        }
+    }, [ciudadOk]);
 
-    const handleChange = (e) => {
-        setCiudad(e.target.value);
+    const consultarAPI = async () => {
+        try {
+            let respuesta = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${ciudadOk}&APPID=370fd6c1a4ad7f1281fc4876c646d91d`);
+            const respuestaClima = await respuesta.json();
+            return respuestaClima;
+        } catch (error) {
+            console.log(error);
+        }
+    };
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        setCiudadOk(ciudad.trim());
+        console.log(ciudadOk);
+        consultarAPI();
     };
 
     return (
@@ -19,20 +54,22 @@ const Formulario = () => {
             <Container>
                 <Row>
                     <Col md={6}>
-                        <Form className="p-2 my-3">
-                            <InputGroup className="mb-3">
-                                <Form.Control
-                                    placeholder="Buscar ciudad"
-                                    aria-label="Recipient's username"
-                                    aria-describedby="basic-addon2"
-                                    onChange={handleChange}
-                                />
-                                <Button type="submit">Buscar</Button>
-                            </InputGroup>
+                        <Form className="p-2 my-3" onSubmit={handleSubmit}>
+                            <Form.Control
+                                name="ciudad"
+                                value={ciudad}
+                                placeholder="Buscar ciudad"
+                                aria-label="Recipient's username"
+                                aria-describedby="basic-addon2"
+                                onChange={(e) => {
+                                    setCiudad(e.target.value);
+                                }}
+                            />
+                            <Button type="submit">Buscar</Button>
                         </Form>
                     </Col>
                 </Row>
-                <Informacion />
+                <Informacion datosClima={datosClima} />
             </Container>
         </>
     );
--- src/components/Informacion.jsx
+++ src/components/Informacion.jsx
@@ -1,6 +1,8 @@
 import { Container, Table, Row, Col } from "react-bootstrap";
 
-const Informacion = () => {
+const Informacion = ({ datosClima }) => {
+    const { coord, weather, main, sys, name } = { ...datosClima };
+
     return (
         <>
             <Container>
@@ -10,35 +12,37 @@ const Informacion = () => {
                             <tbody>
                                 <tr>
                                     <td>Ciudad</td>
-                                    <td>name</td>
+                                    <td>{name}</td>
                                 </tr>
                                 <tr>
                                     <td>Pais</td>
-                                    <td>sys.country</td>
+                                    <td>{sys.country}</td>
                                 </tr>
                                 <tr>
                                     <td>Coordenadas</td>
-                                    <td>Longitud:$coord.lon Latitud:$coord.lat</td>
+                                    <td>
+                                        Longitud: {coord.lon} Latitud: {coord.lat}
+                                    </td>
                                 </tr>
                                 <tr>
                                     <td>Pronostico del Clima</td>
-                                    <td>weather.description</td>
+                                    <td>{weather[0].description}</td>
                                 </tr>
                                 <tr>
                                     <td>Temperatura</td>
-                                    <td>main.temp</td>
+                                    <td>{main.temp}</td>
                                 </tr>
                                 <tr>
                                     <td>Temperatura Max.</td>
-                                    <td>main.temp_max</td>
+                                    <td>{main.temp_max}</td>
                                 </tr>
                                 <tr>
                                     <td>Temperatura Min.</td>
-                                    <td>main.temp_min</td>
+                                    <td>{main.temp_min}</td>
                                 </tr>
                                 <tr>
                                     <td>Sensacion Tèrmica</td>
-                                    <td>main.temp_min</td>
+                                    <td>{main.feels_like}</td>
                                 </tr>
                             </tbody>
                         </Table>
